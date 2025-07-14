8f3dfedff5846f891816543a404e915e3ef2e7d9
Pete Toshkov <top1yok@bosch.com>
2025-07-14 22:23:48 +0100

    added solution for day 20 part 2 example

--- day20.m
+++ day20.m
@@ -164,22 +164,12 @@ function costMat = dijkstra(data, startPos)
     locations = find(data ~= '#');
     costs = Inf*ones(1, numel(locations));
     costs(locations == startPos) = 0;
-    unvisitedLocations = locations;
+    unvisitedLocations = [startPos];
 
     while(~(isempty(unvisitedLocations)))
         % find the unvisited location with the lowest cost
         location = unvisitedLocations(1);
         cost = costs(locations == location);
-
-        for ii = 2:numel(unvisitedLocations)
-            nextLocation = unvisitedLocations(ii);
-            nextCost = costs(locations == nextLocation);
-
-            if(nextCost < cost)
-                location = nextLocation;
-                cost = nextCost;
-            end
-        end
 
         % remove this location from the unvisited locations
         unvisitedLocations(unvisitedLocations == location) = [];
@@ -188,25 +178,37 @@ function costMat = dijkstra(data, startPos)
         upIdx = location - 1;
 
         if(data(upIdx) ~= '#')
-            costs(locations == upIdx) = min(costs(locations == upIdx), cost + 1);
+            if((cost + 1) < costs(locations == upIdx))
+                costs(locations == upIdx) = cost + 1;
+                unvisitedLocations(end + 1) = upIdx;
+            end
         end
 
         downIdx = location + 1;
 
         if(data(downIdx) ~= '#')
-            costs(locations == downIdx) = min(costs(locations == downIdx), cost + 1);
+            if((cost + 1) < costs(locations == downIdx))
+                costs(locations == downIdx) = cost + 1;
+                unvisitedLocations(end + 1) = downIdx;
+            end
         end
 
         leftIdx = location - rows(data);
 
         if(data(leftIdx) ~= '#')
-            costs(locations == leftIdx) = min(costs(locations == leftIdx), cost + 1);
+            if((cost + 1) < costs(locations == leftIdx))
+                costs(locations == leftIdx) = cost + 1;
+                unvisitedLocations(end + 1) = leftIdx;
+            end
         end
 
         rightIdx = location + rows(data);
 
         if(data(rightIdx) ~= '#')
-            costs(locations == rightIdx) = min(costs(locations == rightIdx), cost + 1);
+            if((cost + 1) < costs(locations == rightIdx))
+                costs(locations == rightIdx) = cost + 1;
+                unvisitedLocations(end + 1) = rightIdx;
+            end
         end
     end
 
@@ -216,8 +218,11 @@ end
 
 startPos = find(data == 'S');
 endPos = find(data == 'E');
+
 costMat = dijkstra(data, startPos);
 defaultTime = costMat(endPos)
+% defaultTime = 9452
+
 dataDifferentPadding = data;
 dataDifferentPadding(1, :) = 'X';
 dataDifferentPadding(end, :) = 'X';
@@ -227,18 +232,122 @@ obstacleLocations = find(dataDifferentPadding == '#');
 numel(obstacleLocations)
 sum1 = 0;
 
-for ii = 1:numel(obstacleLocations)
-    ii
-    location = obstacleLocations(ii);
-    newData = data;
-    newData(location) = '.';
-    costMat = dijkstra(newData, startPos);
-    newTime = costMat(endPos);
-    timeDifference = defaultTime - newTime;
+% for ii = 1:numel(obstacleLocations)
+%     ii
+%     location = obstacleLocations(ii);
+%     newData = data;
+%     newData(location) = '.';
+%     costMat = dijkstra(newData, startPos);
+%     newTime = costMat(endPos);
+%     timeDifference = defaultTime - newTime;
+% 
+%     if(timeDifference >= 100)
+%         sum1++;
+%     end
+% end
 
-    if(timeDifference >= 100)
-        sum1++;
+sum1
+% assert(1438 == sum1);
+
+global routeIdxs;
+global routeSubs;
+routeIdxs = [];
+routeSubs = [];
+max_recursion_depth(1024);
+
+function findRoute(costMat, pos, endPos)
+    global routeIdxs;
+    global routeSubs;
+    routeIdxs(end + 1, :) = pos;
+    [a, b] = ind2sub(size(costMat), pos);
+    routeSubs(end + 1, :) = [a, b];
+
+    if(pos == endPos)
+        disp("found")
+        return;
+    end
+
+    currentCost = costMat(pos);
+    upIdx = pos - 1;
+    upCost = costMat(upIdx);
+    downIdx = pos + 1;
+    downCost = costMat(downIdx);
+    rightIdx = pos + rows(costMat);
+    rightCost = costMat(rightIdx);
+    leftIdx = pos - rows(costMat);
+    leftCost = costMat(leftIdx);
+
+    if(currentCost + 1 == upCost)
+        findRoute(costMat, upIdx, endPos)
+    end
+
+    if(currentCost + 1 == downCost)
+        findRoute(costMat, downIdx, endPos)
+    end
+
+    if(currentCost + 1 == rightCost)
+        findRoute(costMat, rightIdx, endPos)
+    end
+
+    if(currentCost + 1 == leftCost)
+        findRoute(costMat, leftIdx, endPos)
     end
 end
 
-sum1
+% findRoute(costMat, startPos, endPos);
+findRoute(costMat, startPos, find(costMat  == 500));
+findRoute(costMat, find(costMat  == 501), find(costMat  == 1000));
+findRoute(costMat, find(costMat  == 1001), find(costMat  == 1500));
+findRoute(costMat, find(costMat  == 1501), find(costMat  == 2000));
+findRoute(costMat, find(costMat  == 2001), find(costMat  == 2500));
+findRoute(costMat, find(costMat  == 2501), find(costMat  == 3000));
+findRoute(costMat, find(costMat  == 3001), find(costMat  == 3500));
+findRoute(costMat, find(costMat  == 3501), find(costMat  == 4000));
+findRoute(costMat, find(costMat  == 4001), find(costMat  == 4500));
+findRoute(costMat, find(costMat  == 4501), find(costMat  == 5000));
+findRoute(costMat, find(costMat  == 5001), find(costMat  == 5500));
+findRoute(costMat, find(costMat  == 5501), find(costMat  == 6000));
+findRoute(costMat, find(costMat  == 6001), find(costMat  == 6500));
+findRoute(costMat, find(costMat  == 6501), find(costMat  == 7000));
+findRoute(costMat, find(costMat  == 7001), find(costMat  == 7500));
+findRoute(costMat, find(costMat  == 7501), find(costMat  == 8000));
+findRoute(costMat, find(costMat  == 8001), find(costMat  == 8500));
+findRoute(costMat, find(costMat  == 8501), find(costMat  == 9000));
+findRoute(costMat, find(costMat  == 9001), endPos);
+
+sum2 = 0;
+
+for ii = 1:(numel(routeIdxs) - 1)
+    startIdx = routeIdxs(ii);
+    startSub = routeSubs(ii, :);
+    startCost = costMat(startIdx);
+
+    for jj = (ii + 1):numel(routeIdxs)
+        endIdx = routeIdxs(jj);
+        endSub = routeSubs(jj, :);
+        endCost = costMat(endIdx);
+
+        costDifference = endCost - startCost;
+
+        % if(costDifference < 50)
+        if(costDifference < 100)
+            continue;
+        end
+
+        % check if this end index is reachable with cheats
+
+        % find the minimum cost to get from start index to end index
+        distance = abs(startSub(1) - endSub(1)) + abs(startSub(2) - endSub(2));
+
+        % compute the cost with cheats = costDifference - minimum cost to get from 
+        costSaving = costDifference - distance;
+
+        % if(costSaving >= 50)
+        if(costSaving >= 100)
+            sum2++;
+        end
+    end
+end
+
+sum2
+
